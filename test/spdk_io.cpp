4316adf7e539b7232a48607893f710b7e586c09c
JerryMouseZ <1711333@mail.nankai.edu.cn>
2023-11-02 14:01:13 +0800

    unkown error in write

---
+++ test/spdk_io.cpp
@@ -0,0 +1,25 @@
+#include "service.hpp"
+#include "spdk/env.h"
+#include "task.hpp"
+#include <catch2/catch_all.hpp>
+#include <catch2/catch_test_macros.hpp>
+const char *json_file = "bdev.json";
+const char *bdev_dev = "Malloc0";
+
+task<int> simple_write_read(spdk_service *service) {
+  // 不用担心，因为这个协程肯定是执行在spdk线程里的
+  char *dma_buf = (char *)spdk_dma_zmalloc(4096, 4096, nullptr);
+  strcpy(dma_buf, "hello world");
+  int rc = co_await service->write(dma_buf, 4096, 0);
+  REQUIRE(rc == 0);
+  memset(dma_buf, 0, 4096);
+  rc = co_await service->read(dma_buf, 4096, 0);
+  REQUIRE(rc == 0);
+  REQUIRE(strcmp(dma_buf, "hello world") == 0);
+  co_return 0;
+}
+
+TEST_CASE("simple_io", "simple_write_read") {
+  spdk_service *service = new spdk_service(1, json_file, bdev_dev);
+  service->run(simple_write_read(service));
+}
