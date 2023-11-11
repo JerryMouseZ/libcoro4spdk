3b423419bae0e3f907666c778fcfb5b3be98e54e
JerryMouseZ <1711333@mail.nankai.edu.cn>
2023-11-11 10:57:35 +0800

    change default compiler to clang++ to fix the problem

---
+++ test/empty_loop.cpp
@@ -0,0 +1,20 @@
+#include "task.hpp"
+#include <assert.h>
+
+task<int> empty() { co_return 2; }
+task<int> empty_loop() {
+  int r = 0;
+  for (int i = 0; i < 100000; ++i) {
+    r += co_await empty();
+  }
+  co_return r;
+}
+
+int main(int argc, char *argv[]) {
+  auto t = empty_loop();
+  t.start();
+
+  assert(t.get().value() == 100000 * 2);
+
+  return 0;
+}
