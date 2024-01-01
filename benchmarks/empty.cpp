a16dd901f2ed51546bfb9fcf5231484c61516738
jz <jiangzhangsg@qq.com>
2024-01-01 16:07:56 +0800

    update brenchmarks: 1. add posix locking benchmark; 2. make colocking benchmark more accurate

--- benchmarks/empty.cpp
+++ benchmarks/empty.cpp
@@ -1,7 +1,11 @@
 #include <atomic>
 #include <cstdio>
 #include <cstdlib>
+#include <bits/getopt_core.h>
+#include <bits/types/struct_timeval.h>
 #include <sys/time.h>
+#include <unistd.h>
+#include <ctime>
 #include <iostream>
 #include <vector>
 #include "task.hpp"
@@ -10,19 +14,45 @@
 #include "rcu.hpp"
 #include "mutex.hpp"
 
+enum LockType { Mutex, SpinLock, RwLock, RCU };
+std::vector<std::string> locktypes = {"mutex", "spinlock", "rwlock", "rcu"};
+LockType type;
+int num_readers;
+int num_writers;
+int iterations;
+
 int val = 0;
+int thread_num = 1;
+std::atomic<int> ongoing = 0;
+timeval begin;
+timeval end;
+timeval tbegin, tend;
+
+void print_result(timeval begin, timeval end) {
+  printf("type: %s\tnum_readers: %d\tnumwriters: %d\titerations: %d\n",
+         locktypes[type].c_str(), num_readers, num_writers, iterations);
+  double elapse = (end.tv_sec - begin.tv_sec);
+  elapse += double(end.tv_usec - begin.tv_usec) / 1000000;
+  printf("%lf s\n", elapse);
+}
 
 task<int> reader(int index, int loop, async_simple::coro::Mutex& lock) {
+  while (ongoing == 0)
+    ;
+  gettimeofday(&tbegin, nullptr);
   int res = 0;
   for (int i = 0; i < loop; ++i) {
     co_await lock.coLock();
     res += val;
     lock.unlock();
   }
+  gettimeofday(&tend, nullptr);
   co_return res;
 }
 
 task<int> reader(int index, int loop, async_simple::coro::SpinLock& lock) {
+  while (ongoing == 0)
+    ;
   int res = 0;
   for (int i = 0; i < loop; ++i) {
     co_await lock.coLock();
@@ -33,6 +63,8 @@ task<int> reader(int index, int loop, async_simple::coro::SpinLock& lock) {
 }
 
 task<int> reader(int index, int loop) {
+  while (ongoing == 0)
+    ;
   int res = 0;
   for (int i = 0; i < loop; ++i) {
     pmss::rcu::rcu_read_lock();
@@ -44,6 +76,8 @@ task<int> reader(int index, int loop) {
 
 template <typename LockType>
 task<int> writer(int index, int loop, LockType& lock) {
+  while (ongoing == 0)
+    ;
   int res = 0;
   for (int i = 0; i < loop; ++i) {
     co_await lock.coLock();
@@ -55,6 +89,8 @@ task<int> writer(int index, int loop, LockType& lock) {
 
 async_simple::coro::SpinLock rcu_spinlock;
 task<int> writer(int index, int loop) {
+  while (ongoing == 0)
+    ;
   int res = 0;
   for (int i = 0; i < loop; ++i) {
     co_await rcu_spinlock.coLock();
@@ -67,25 +103,19 @@ task<int> writer(int index, int loop) {
   co_return 0;
 }
 
-enum LockType { Mutex, SpinLock, RwLock, RCU };
-std::vector<std::string> locktypes = {"mutex", "spinlock", "rwlock", "rcu"};
-LockType type;
-int num_readers;
-int num_writers;
-int iterations;
-
 void args_parse(int argc, char** argv) {
   if (argc < 2) {
     fprintf(stderr,
             "usage: %s -t [mutex/spinlock/sharedmutex/rcu] -r [reader num] "
             "-w [writer num] "
+            "-c [core num]"
             "-i iterations\n",
             argv[0]);
     exit(-1);
   }
 
   int c;
-  while ((c = getopt(argc, argv, "t:r:w:i:")) != -1) {
+  while ((c = getopt(argc, argv, "t:r:w:i:c:")) != -1) {
     switch (c) {
       case 't':
         if (optarg[0] == 'm') {
@@ -107,16 +137,12 @@ void args_parse(int argc, char** argv) {
         break;
       case 'i':
         iterations = atoi(optarg);
+        break;
+      case 'c':
+        thread_num = atoi(optarg);
+        break;
     }
   }
-}
-
-void print_result(timeval begin, timeval end) {
-  printf("type: %s\tnum_readers: %d\tnumwriters: %d\titerations: %d\n",
-         locktypes[type].c_str(), num_readers, num_writers, iterations);
-  double elapse = (end.tv_sec - begin.tv_sec);
-  elapse += double(end.tv_usec - begin.tv_usec) / 1000000;
-  printf("%lf s\n", elapse);
 }
 
 void run_rcu() {
@@ -127,15 +153,12 @@ void run_rcu() {
   pmss::run();
 }
 
-int main(int argc, char* argv[]) {
-  args_parse(argc, argv);
-  pmss::init_service(16, "bdev.json", "Malloc0");
+void benchmark_thread() {
+  pmss::init_service(thread_num, "bdev.json", "Malloc0");
 
-  timeval begin;
   async_simple::coro::Mutex mutex;
   async_simple::coro::SpinLock spinlock;
 
-  gettimeofday(&begin, nullptr);
   if (type == RCU) {
     run_rcu();
     goto done;
@@ -158,10 +181,19 @@ int main(int argc, char* argv[]) {
   pmss::run();
 
 done:
-  timeval end;
   gettimeofday(&end, nullptr);
-  print_result(begin, end);
+  print_result(begin, tbegin);
+  print_result(tbegin, tend);
+  print_result(tend, end);
   pmss::deinit_service();
+}
 
+int main(int argc, char* argv[]) {
+  args_parse(argc, argv);
+  std::thread t(benchmark_thread);
+  sleep(1);
+  ongoing = 1;
+  gettimeofday(&begin, nullptr);
+  t.join();
   return 0;
 }
